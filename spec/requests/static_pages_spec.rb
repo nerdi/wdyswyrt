bc8418b0d69a2c843de0e10273519c16de978422
dan@nerdi.com <dan@nerdi.com>
2013-10-29 08:45:43 +1100

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) { "Say What You Really Think" }
+  
   describe "Home page" do
     it "should have the content 'Say What You Really Think'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -9,7 +11,7 @@ describe "StaticPages" do
     end
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Say What You Really Think | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
   describe "Help page" do
@@ -19,7 +21,7 @@ describe "StaticPages" do
     end
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Say What You Really Think | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
   describe "About page" do
@@ -30,7 +32,18 @@ describe "StaticPages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Say What You Really Think | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
